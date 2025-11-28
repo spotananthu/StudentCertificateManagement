d1619e08fcd06fc40cad0c069bbaa7411f988ccf
soujanya09 <reddeboinasoujanya@gmail.com>
2025-11-28 17:35:49 +0530

    added new service called emailnotification with required dependencies

---
+++ backend/email-notification/src/main/java/com/email_notification/email_notification/EmailNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.email_notification.email_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailNotificationApplication.class, args);
+	}
+
+}
